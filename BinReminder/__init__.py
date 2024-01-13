6ffc59c0ac07a950232a846aa18916638b37988d
nickcosta <nicholaspcosta@gmail.com>
2024-01-13 15:10:30 +0000

    fixed errors

--- BinReminder/__init__.py
+++ BinReminder/__init__.py
@@ -38,16 +38,14 @@ def send_sms(message, numbers):
     """
     client = Client(account_sid, auth_token)
     for number in numbers:
-                try:
+        try:
             message = client.messages.create(
-                to=number, 
-                from_=twilio_phone_number, 
-                body=message
+                to=number, from_=twilio_phone_number, body=message
             )
             print(f"SMS Sent to: {number}, SID: {message.sid}")
         except Exception as e:
             print(f"Failed to send SMS to {number}: {e}")
-        print("SMS Sent to:", numbers)
+    print("SMS Sent to:", numbers)
 
 
 def check_and_send_sms():
